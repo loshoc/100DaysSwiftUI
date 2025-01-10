40579906afafcf88a567acf412dfe255e7148fb5
Kiwi Guo <loshochung@hotmail.com>
2025-01-10 20:53:39 +0800

    Initial Commit

---
+++ 100DaysSwiftUI/_00DaysSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysSwiftUIApp.swift
+//  100DaysSwiftUI
+//
+//  Created by Kiwi Guo on 2025/1/10.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
